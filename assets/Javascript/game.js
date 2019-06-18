0b20f2d21b3c6cfe3b1f3450dab329d1a955cc56
Zac Morton <zmorton8057@gmail.com>
2019-06-17 21:24:20 -0700

    Added variables from API

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -2,4 +2,57 @@ var triviaObject = []
 var correctCounter = 0;
 var incorrectCounter = 0;
 var question = [];
-var multipleChoice = [];
+var incorrect = [];
+var correct = [];
+var answerArray = [];
+var percentageCorrect = [];
+
+
+/// grab trivia quesions from a API, 
+axios({
+    url: "https://opentdb.com/api.php?amount=20&category=21&difficulty=medium&type=multiple",
+    method: "GET"
+})
+.then(function(response){
+    for(i = 0; i < response.data.results.length; i++){
+    question = response.data.results[i].question
+    incorrect = response.data.results[i].incorrect_answers
+    correct = response.data.results[i].correct_answer   
+    }
+    console.log(response.data.results)
+    console.log(question)
+    console.log(incorrect)
+    console.log(correct)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+})
+///append questions to the buttons, 
+
+//if correct answer is selected correctCounter++;   
+
+///else if answer is incorrect incorrectCounter++
+
+//// number of correct selected "/" the number of questions answered (incorrectCounter + correctCounter)
+
+/// append div with new buttons/questions
