bbc9987a827040d6d00b2362797f26c32f74daeb
Zac Morton <zmorton8057@gmail.com>
2019-06-19 22:05:40 -0700

    Updated Styling push variables to an array

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -6,23 +6,34 @@ var incorrect = [];
 var correct = [];
 var answerArray = [];
 var percentageCorrect = [];
+var emptyArray = []
 
-
-/// grab trivia quesions from a API, 
+/// grab trivia questions from a API, 
 axios({
     url: "https://opentdb.com/api.php?amount=20&category=21&difficulty=medium&type=multiple",
     method: "GET"
 })
-.then(function(response){
-    for(i = 0; i < response.data.results.length; i++){
-    question = response.data.results[i].question
-    incorrect = response.data.results[i].incorrect_answers
-    correct = response.data.results[i].correct_answer   
+    .then(function (response) {
+        for (i = 0; i < response.data.results.length; i++) {
+            question = response.data.results[i].question
+            incorrect = response.data.results[i].incorrect_answers
+            correct = response.data.results[i].correct_answer
+            answerArray = incorrect
+            answerArray.push(correct)
+            
+        } 
+        for (i = 0; i < answerArray.length; i++){
+        
+        $(".answerDiv").append("<input type='button' id='answerButton' class=" + answerArray[i] + " value=" + answerArray[i] + "></input>").attr(answerArray[i])
+          
     }
-    console.log(response.data.results)
-    console.log(question)
-    console.log(incorrect)
-    console.log(correct)
+        console.log(response.data.results)
+        console.log(question)
+        console.log(incorrect)
+        console.log(correct)
+        console.log(answerArray)
+        console.log(typeof(response.data.results[i].incorrect_answers))
+        
 
 
 
@@ -44,10 +55,10 @@ axios({
 
 
 
-    
 
-})
-///append questions to the buttons, 
+
+    })
+///append questions to the buttons,
 
 //if correct answer is selected correctCounter++;   
 
