27b0fe03616bbebd9215843507f0a93efe79decb
Zac Morton <zmorton8057@gmail.com>
2019-06-22 17:48:07 -0700

    On button click, goes to the next question

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -7,6 +7,7 @@ var correct = [];
 var answerArray = [];
 var percentageCorrect = [];
 
+
 function shuffle(a) {
     var j, x, i;
     for (i = a.length - 1; i > 0; i--) {
@@ -24,68 +25,91 @@ axios({
     url: "https://opentdb.com/api.php?amount=20&category=21&difficulty=medium&type=multiple",
     method: "GET"
 })
-    .then(function(response) {
+    .then(function (response) {
         shuffle(response.data.results)
-       
-        var resetFunc = function(){
-////question number start at zero begins at the array when gets to next function increment the variable and then update the DOM
-        for (i = 0; i < response.data.results.length; i++) {
-            ///loops through the object to return the question
-            
-            
-            question = response.data.results[i].question
-            incorrect = response.data.results[i].incorrect_answers
-            correct = response.data.results[i].correct_answer
-            answerArray = incorrect
-            answerArray.push(correct)
-            shuffle(answerArray)
-            
-        }
-}
-            resetFunc()
+        var j = 0;
+        function resetVar(){
+        ////question number start at zero begins at the array when gets to next function increment the variable and then update the DOM
+        
+        
+        ///loops through the object to return the question
+        console.log(response.data.results[0].question)
+        console.log(response.data.results[1].question)
+        console.log(response.data.results[3].question)
+        question = response.data.results[j].question
 
-        var resetAnswer = function(){
+        incorrect = response.data.results[j].incorrect_answers
+        correct = response.data.results[j].correct_answer
+        answerArray = incorrect
+        answerArray.push(correct)
+        shuffle(answerArray)
+        j++
+        }
+
+        resetVar();
+
+
+
         for (var i = 0; i < answerArray.length; i++) {
-            
+
             $(".answerDiv").append("<div>" + answerArray[i] + "</div>")
-            
-            }
+
         }
 
-            resetAnswer()
-        var resetDOM = function(){
-         /// prints question to the dom
-         for(var i=0; i< question.length; i++)
-         $("#questionDiv").text(question[i])
-         console.log(question[i])
-        }
-         /// prints correctCounter to the dom
-         $(".correct").text("Correct:" + correctCounter)
- 
-         /// prints incorrectCounter to the Dom
-         $(".incorrect").text("Incorrect:" + incorrectCounter)
-            
-            resetDOM()
+
+
+
+        /// prints question to the dom
+
+        $("#questionDiv").append("<div>" + question + "</div>")
+
+
+
+        /// prints correctCounter to the dom
+        $(".correct").text("Correct:" + correctCounter)
+
+        /// prints incorrectCounter to the Dom
+        $(".incorrect").text("Incorrect:" + incorrectCounter)
+
+
 
         /////////////////Sets Timer Interval
-       
-         
+
+
         ////assigning click value to each button... then check against the correct array and return true or false   
-        $('.answerDiv').click(function(e) {
+        $('.answerDiv').click(function (e) {
             if (e.target.innerText === correct) {
                 correctCounter++
-                $(".correct").text("Correct:" + correctCounter)
-                response.data.results[i]++
-                resetFunc()
-                resetAnswer()
-                resetDOM()
+                $(".correct").text("Correct: " + correctCounter)
+
+                j++
+                resetVar()    
+
+                
+                for (var i = 0; i < answerArray.length; i++) {
+
+                    $(".answerDiv").append("<div>" + answerArray[i] + "</div>")
+
+                }
+                $("#questionDiv").append("<div>" + question + "</div>")
+
+
+
+
             } else {
                 incorrectCounter++
-                $(".incorrect").text("Incorrect:" + incorrectCounter)
-                response.data.results[i]++
-                resetFunc()
-                resetAnswer()
-                resetDOM()
+                $(".incorrect").text("Incorrect: " + incorrectCounter)
+                
+                j++
+                resetVar()
+
+                for (var i = 0; i < answerArray.length; i++) {
+
+                    $(".answerDiv").append("<div>" + answerArray[i] + "</div>")
+
+                }
+                $("#questionDiv").append("<div>" + question + "</div>")
+
             }
         });
 
@@ -98,7 +122,7 @@ axios({
         console.log(incorrect)
         console.log(correct)
         console.log(answerArray)
-        
+
 
 
 
